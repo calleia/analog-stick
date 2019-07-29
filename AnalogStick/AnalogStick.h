06786aadf388d961c748f71e9d9253bed9c03cb5
Fellipe Calleia <fellipe@calleia.com.br>
2019-07-29 14:14:23 -0300

    Create Xcode project

---
+++ AnalogStick/AnalogStick.h
@@ -0,0 +1,19 @@
+//
+//  AnalogStick.h
+//  AnalogStick
+//
+//  Created by Fellipe Calleia on 7/29/19.
+//  Copyright © 2019 Fellipe Calleia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalogStick.
+FOUNDATION_EXPORT double AnalogStickVersionNumber;
+
+//! Project version string for AnalogStick.
+FOUNDATION_EXPORT const unsigned char AnalogStickVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalogStick/PublicHeader.h>
+
+
